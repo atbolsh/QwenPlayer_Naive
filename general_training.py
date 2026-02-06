ea6953b7cef948577fb2c195ab099a0f82f43534
Artem Bolshakov <a.t.bolsh@gmail.com>
2026-02-06 16:03:37 -0500

    msg passing

--- general_training.py
+++ general_training.py
@@ -377,7 +377,7 @@ def main():
     parser = argparse.ArgumentParser(description="Train QwenAgentPlayer")
     parser.add_argument("--use_lora", action="store_true", help="Use LoRA adapters")
     parser.add_argument("--num_batches", type=int, default=10000000000, help="Number of training batches")
-    parser.add_argument("--batch_size", type=int, default=12, help="Batch size")
+    parser.add_argument("--batch_size", type=int, default=70, help="Batch size")
     parser.add_argument("--lr", type=float, default=1e-5, help="Learning rate")
     parser.add_argument("--save_every", type=int, default=1000, help="Save checkpoint every N batches")
     parser.add_argument("--print_every", type=int, default=100, help="Print progress every N batches")
