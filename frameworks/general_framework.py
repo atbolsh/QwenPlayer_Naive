340bb98885b1d912d00a05ea25d9cc9bb23158f2
Artem Bolshakov <a.t.bolsh@gmail.com>
2026-02-20 12:58:02 -0500

    added label smoothing; other anti-overfitting techniques can wait until I feel they're necessary (probably next week)

--- frameworks/general_framework.py
+++ frameworks/general_framework.py
@@ -302,7 +302,10 @@ def model_forward_with_tokens(model, text_batch, img_batch, ret_imgs=True,
 # Loss functions
 ########
 
-ent_criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id if tokenizer.pad_token_id is not None else 0)
+ent_criterion = nn.CrossEntropyLoss(
+    ignore_index=tokenizer.pad_token_id if tokenizer.pad_token_id is not None else 0,
+    label_smoothing=0.1,
+)
 
 def get_text_loss(res, inputs):
     """Compute cross-entropy loss for text prediction."""
